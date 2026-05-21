126660cc3999f9efb8b65e98e0ddf519e9817dc7
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-05-21 11:22:28 -0300

    feat: checa se o arquivo .txt existe antes de tentar ler os produtos

--- src/services/TxtService.ts
+++ src/services/TxtService.ts
@@ -1,5 +1,21 @@
 import appConfig from "../config/app.config.js";
+import { access } from "node:fs/promises";
+import { resolve } from "node:path";
 
 export default class TxtService {
-  static async fetchProductsTxt() {}
+  private static async checkFileExists(): Promise<boolean> {
+    try {
+      const filePath = resolve(appConfig.txtFilePath);
+      await access(filePath);
+      return true;
+    } catch (error) {
+      console.error("Error checking file existence:", error);
+      return false;
+    }
+  }
+
+  static async fetchProductsTxt() {
+    const fileExists = await this.checkFileExists();
+    console.log("File exists:", fileExists);
+  }
 }
