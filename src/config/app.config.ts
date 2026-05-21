d32135befe0524a32019c7c25f0cb9231daeebe9
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-05-21 11:17:30 -0300

    feat: cria as configs para o caminho do arquivo

---
+++ src/config/app.config.ts
@@ -0,0 +1,4 @@
+export default {
+  txtFilePath: "C:/Users/Victor.DESKTOP-C4LD4B4/Desktop/20.03/Fotos.txt",
+  photosDownloadPath: "C:/Users/Victor.DESKTOP-C4LD4B4/Desktop/20.03",
+};
--- src/services/TxtService.ts
+++ src/services/TxtService.ts
@@ -1,5 +1,5 @@
+import appConfig from "../config/app.config.js";
+
 export default class TxtService {
-  static async fetchProductsTxt() {
-    console.log("Buscando produtos no arquivo .txt...");
-  }
+  static async fetchProductsTxt() {}
 }
