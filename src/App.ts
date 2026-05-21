06a8412fc4427fdad441bbf28f07b134d2c7d7bd
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-05-21 10:56:23 -0300

    refactor: cria fluxograma para o processo de download de fotos

---
+++ src/App.ts
@@ -0,0 +1,10 @@
+export default class App {
+  async start() {
+    // Pegar os produtos da Grendene
+    // função
+    // Processar os produtos
+    // função
+    // Baixar as fotos
+    // função
+  }
+}
