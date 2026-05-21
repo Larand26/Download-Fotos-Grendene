5002095dc747035921ea693fe6617ce690129922
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-05-21 12:33:45 -0300

    feat: faz os loops de dowload de fotos

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,4 +1,5 @@
 export default {
   txtFilePath: "C:/Users/Victor.DESKTOP-C4LD4B4/Desktop/20.03/Fotos.txt",
   photosDownloadPath: "C:/Users/Victor.DESKTOP-C4LD4B4/Desktop/20.03",
+  maxPhotoPositions: 5,
 };
--- src/services/PhotosService.ts
+++ src/services/PhotosService.ts
@@ -1,7 +1,44 @@
 import type { iProduct, iResponse } from "../interfaces/app.interfaces.js";
+import appConfig from "../config/app.config.js";
+import axios from "axios";
 
 export default class PhotosService {
+  private static async downloadPhoto(
+    manufactureCode: string,
+    colorCode: string,
+    position: string,
+  ): Promise<iResponse> {
+    // função para baixar a foto
+    return {
+      success: true,
+      message: `Foto baixada: ${manufactureCode}-${colorCode}-${position}`,
+    };
+  }
+
   static async downloadPhotos(products: iProduct[]): Promise<iResponse> {
+    if (products.length === 0) {
+      return {
+        success: false,
+        message: "No products to download photos for.",
+      };
+    }
+
+    for (const product of products) {
+      for (const colorCode of product.colorCode) {
+        for (
+          let position = 0;
+          position < appConfig.maxPhotoPositions;
+          position++
+        ) {
+          const response = await this.downloadPhoto(
+            product.manufactureCode,
+            colorCode,
+            position.toString().padStart(2, "0"),
+          );
+          if (!response.success) continue;
+        }
+      }
+    }
     return {
       success: true,
       data: products,
